50247350c53c67056ab3efffe4e355c517fe04d5
Oxhaider <arulselvan2244@gmail.com>
2025-04-12 00:14:54 +0530

    Update backend

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+
+
 
 app.use(cors());
 
